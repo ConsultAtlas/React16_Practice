9ccc7acc7b8ac7a0f1141ae7d4b8fba35283ee9c
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-12-30 12:45:30 -0500

    added inout field and user input validation message.

--- react-conditionals-and-lists/src/App.js
+++ react-conditionals-and-lists/src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
+  
+  state = {
+    userInput: ''
+  }
+  // User Input Field Event Handler
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.value})
+  }
+  
   render() {
     return (
       <div className="App">
@@ -13,6 +23,13 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <hr />
+        <input 
+          type="text" 
+          onChange={this.inputChangedHandler} 
+          value={this.state.userInput}/>
+        <p>{this.state.userInput}</p>
+        <Validation inputLength={this.state.userInput.length}/>
       </div>
     );
   }
