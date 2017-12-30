19ee1cb017dd34310d8ea7337575353844e03012
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-12-30 13:32:21 -0500

    added the delete character handler to delete characters we click on

--- react-conditionals-and-lists/src/App.js
+++ react-conditionals-and-lists/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Validation from './Validation/Validation';
+import CharComponent from './CharComponent/CharComponent';
 
 class App extends Component {
   
@@ -12,24 +13,39 @@ class App extends Component {
   inputChangedHandler = (event) => {
     this.setState({userInput: event.target.value})
   }
+
+  // deletes characters of text that we click on. Takes an input of index.
+  deleteCharHandler = ( index ) => {
+    // makes a copy of the userInput property of state and splits it into an array
+    // of individual characters.
+    const text = this.state.userInput.split('');
+    // removes 1 character from the text[] at the index of 'index'.
+    text.splice( index , 1 );
+    // creates a new array updatedText[] and then joins the array back into one string.
+    const updatedText = text.join('');
+    // Sets the state property of UserInput to the the new string we made, updatedText
+    // which has the characters we clicked on deleted from the string.
+    this.setState({userInput: updatedText});
+  }
   
   render() {
+
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <CharComponent 
+                character={ch} 
+                key={index}
+                clicked={() => this.deleteCharHandler(index)}/>;
+    });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <hr />
+      <div>
         <input 
           type="text" 
           onChange={this.inputChangedHandler} 
           value={this.state.userInput}/>
         <p>{this.state.userInput}</p>
         <Validation inputLength={this.state.userInput.length}/>
+        {charList}
       </div>
     );
   }
